00bd6c4e96a8de4bb17c74d29a553c39f671c9c5
Stefan Kaes <stefan.kaes@xing.com>
2013-06-07 12:24:04 +0200

    passenger4 compatibility

--- init.rb
+++ init.rb
@@ -1,13 +1,19 @@
 if defined?(PhusionPassenger)
-  class PhusionPassenger::AbstractRequestHandler
-    alias_method :original_reset_signal_handlers, :reset_signal_handlers
+  if PhusionPassenger::VERSION_STRING < '4.0'
+    class PhusionPassenger::AbstractRequestHandler
+      alias_method :original_reset_signal_handlers, :reset_signal_handlers
 
-    def reset_signal_handlers
-      original_reset_signal_handlers
+      def reset_signal_handlers
+        original_reset_signal_handlers
+        GCHacks.install_signal_handlers
+      end
+
+      protected :original_reset_signal_handlers, :reset_signal_handlers
+    end
+  else
+    PhusionPassenger.on_event(:after_installing_signal_handlers) do
       GCHacks.install_signal_handlers
     end
-
-    protected :original_reset_signal_handlers, :reset_signal_handlers
   end
 
 else
